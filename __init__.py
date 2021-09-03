5cbcb56b3686defc1125c779d7c4270672a6cce5
bobir01 <bobirtemka123@gmail.com>
2021-09-03 22:14:25 +0500

    last change

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from . import db_api
+from . import misc
+from .notify_admins import on_startup_notify
+from .cc_checker import card_checker	
--- data/config.py
+++ data/config.py
@@ -11,11 +11,3 @@ ADMINS = ['835282186'] # adminlar ro'yxati
 IP = 'localhost'  # Xosting ip manzili
 
 
-
-
-logging.info(msg=BOT_TOKEN	)
-"""
-ADMINS=835282186
-BOT_TOKEN=1904606439:AAFAY42P1hwd4hxE2J9ITcM-kfNpvdbFtNw
-ip=localhost
-"""
